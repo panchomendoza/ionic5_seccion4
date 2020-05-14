7c0fe45b92bcf41d8b33a83983930825272328a5
jmendoza <juan.mendoza@karibu.cl>
2020-05-14 16:10:34 -0400

    se agrega skeleton a segment

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Componente } from '../interfaces/interfaces';
 
+import { delay } from 'rxjs/operators'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -22,7 +24,10 @@ export class DataService {
   }
 
   getSuperHeroes(){
-    return this.http.get('/assets/data/superheroes.json');
+    return this.http.get('/assets/data/superheroes.json')
+      .pipe(
+        delay(1500)
+      );
   }
 
 }
