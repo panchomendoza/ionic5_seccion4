51bc63cc398c75504c8cc1fa2fbe2a2907a911e2
jmendoza <juan.mendoza@karibu.cl>
2020-05-15 00:10:12 -0400

    se agrega toast

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Observable } from 'rxjs';
-import { IonList } from '@ionic/angular';
+import { IonList, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -14,7 +14,7 @@ export class ListPage implements OnInit {
 
   usuarios: Observable<any>;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private toastCtrl: ToastController) { }
 
   ngOnInit() {
 
@@ -22,18 +22,29 @@ export class ListPage implements OnInit {
     
   }
 
+  async presentToast(message:string) {
+    const toast = await this.toastCtrl.create({
+      message, // ECM6 
+      duration: 2000
+    });
+    toast.present();
+  }
+
   favorite(user){
-    console.log('favorite ',user);
+    //console.log('favorite ',user);
+    this.presentToast('Guardo en favoritos');
     this.lista.closeSlidingItems();
   }
 
   share(user){
-    console.log('share ',user);
+    //console.log('share ',user);
+    this.presentToast('Compartido');
     this.lista.closeSlidingItems();
   }
 
   borrar(user){
-    console.log('borrar ',user);
+    //console.log('borrar ',user);
+    this.presentToast('Borrar');
     this.lista.closeSlidingItems();
   }
 
